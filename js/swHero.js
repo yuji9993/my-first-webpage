9323192df9717eed630babb7ef9aa99cfc48ee41
yuji9993 <yuji9993@gmail.com>
2026-09-07 16:41:49 +0900

    스와이퍼 구현중, slideChangeTransitionStart 메서드 구현

--- js/swHero.js
+++ js/swHero.js
@@ -1,4 +1,14 @@
+const mainVisualBg = document.querySelector(
+  ".hero .main-visual .main-visual-bg",
+);
+// console.log(mainVisualBg);
+
 const swHero = new Swiper(".sw-hero", {
+  speed: 1000,
+  // autoplay: {
+  //   delay: 5000,
+  //   disableOnInteraction: false,
+  // },
   pagination: {
     el: ".swiper-pagination",
     type: "fraction",
@@ -6,5 +16,18 @@ const swHero = new Swiper(".sw-hero", {
   navigation: {
     nextEl: ".hero-next-btn",
     prevEl: ".hero-prev-btn",
+  },
+
+  on: {
+    slideChangeTransitionStart: function (swiper) {
+      // const activeSilde = swiper.activeIndex + 1;
+      // console.log(activeSilde);
+      // mainVisualBg.style.backgroundImage = `url(assets/images/slide_${activeSilde}.png)`;
+      const activeslide = swiper.slides[swiper.activeindex];
+      // console.log(activeslide);
+      const activeslideimg = activeslide.queryselector(".slide-image img");
+      // console.log(activeslideimg.src);
+      mainVisualBg.style.backgroundimage = `url(${activeslideimg.src})`;
+    },
   },
 });
