22beb083dfea6e9b94bac829a5cb7e835b1fe792
yuji9993 <yuji9993@gmail.com>
2026-09-10 17:21:16 +0900

    추천 축제, 많이본 축제 영역

--- js/swHero.js
+++ js/swHero.js
@@ -7,14 +7,14 @@ const heroBtns = document.querySelectorAll(".visual-inner .sw-hero-btn");
 const playStopBtn = document.querySelector(
   ".sw-hero .pagination .play-stop-btn",
 );
-console.log(playStopBtn);
+// console.log(playStopBtn);
 
-// 히어로 슬라이드 버튼 호버 인터렉션
+// 히어로 슬라이드 버튼 호버 인터랙션
 heroBtns.forEach((btn) => {
   btn.addEventListener("mouseenter", () => {
     btn.querySelector(".off").style.opacity = 1;
   });
-  btn.addEventListener("mouseout", () => {
+  btn.addEventListener("mouseleave", () => {
     btn.querySelector(".off").style.opacity = 0;
   });
 });
@@ -37,31 +37,35 @@ const swHero = new Swiper(".sw-hero", {
 
   on: {
     slideChangeTransitionStart: function (swiper) {
-      // const activeSlide = swiper.activeIndex + 1;
-      // console.log(activeSlide);
-      // mainVisualBg.style.backgroundImage = `url("../assets/images/slide_${activeSlide}.png")`;
+      // const activeSilde = swiper.activeIndex + 1;
+      // console.log(activeSilde);
+      // mainVisualBg.style.backgroundImage = `url(assets/images/slide_${activeSilde}.png)`;
       const activeSlide = swiper.slides[swiper.activeIndex];
       // console.log(activeSlide);
-      const activeSlideImage = activeSlide.querySelector(".slide-image img");
-      // console.log(activeSlideImage.src);
-      mainVisualBg.style.backgroundImage = `url(${activeSlideImage.src})`;
+      const activeSlideImgUrl =
+        activeSlide.querySelector(".slide-image img").src;
+      // console.log(activeSlideImg.src);
+      mainVisualBg.style.backgroundImage = `url(${activeSlideImgUrl})`;
     },
   },
 });
-// new 함수 호출하는데 연산자 new 써서 호출하면 객체{}를 생성해준다? => 앞글자 대문자는 생성자 함수
 
-// 재생/정지 버튼
+// 재생, 정지 버튼
 playStopBtn.addEventListener("click", () => {
+  // console.log("슬라이드 자동재생 정지");
+
   const isRunning = swHero.autoplay.running;
   // console.log(isRunning);
 
   if (isRunning) {
     swHero.autoplay.stop();
-    console.log("정지");
-    playStopBtn.style.backgroundImage = `url("../assets/icons/slide_play.png")`;
+    console.log("자동재생 정지");
+    playStopBtn.style.backgroundImage = `url(assets/icons/slide_play.png)`;
+    playStopBtn.textContent = "재생";
   } else {
     swHero.autoplay.start();
-    console.log("출발");
-    playStopBtn.style.backgroundImage = `url("../assets/icons/slide_stop.png")`;
+    console.log("자동재생 시작");
+    playStopBtn.style.backgroundImage = `url(assets/icons/slide_stop.png)`;
+    playStopBtn.textContent = "정지";
   }
 });
