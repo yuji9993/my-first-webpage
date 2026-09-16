cef009660827273911c695e61f914b89d2441c3b
yuji9993 <yuji9993@gmail.com>
2026-09-16 16:52:22 +0900

    지역 축제 영역 완성

--- js/swFestival.js
+++ js/swFestival.js
@@ -1,11 +1,12 @@
 const swFestival = new Swiper(".sw-festival", {
+  speed: 350,
   loop: true,
   slidesPerView: 3.5,
-  slidesPerGroup: 1,
+  slidespergroup: 1,
   spaceBetween: 32,
   //   centeredSlides: true,
   pagination: {
-    el: ".festival .pagination .swiper-pagination",
+    el: ".festival .swiper-pagination",
     type: "fraction",
   },
   navigation: {
